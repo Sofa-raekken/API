c1cd0ced203693d81b70c7f42bbc884339a1bacc
Long Yong Ly <long0020@edu.sde.dk>
2022-04-27 15:30:40 +0200

    deleted Weatherforecast and disabled UseDeveloperExceptionPage on Production

--- ZooAPI/Startup.cs
+++ ZooAPI/Startup.cs
@@ -70,9 +70,8 @@ namespace ZooAPI
         {
             if (env.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
             }
-            app.UseDeveloperExceptionPage();
-
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ZooAPI v1"));
 
